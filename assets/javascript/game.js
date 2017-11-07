b8fc1db005bfd8483ceb04b14c67986e1f327f35
Diana <dianaltecson@gmail.com>
2017-11-07 08:14:40 -0800

    created starter files

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAA5loVvVusMCIaHbr3gNRrym0yoCO68hY",
+	authDomain: "rps-multiplayer-72abc.firebaseapp.com",
+	databaseURL: "https://rps-multiplayer-72abc.firebaseio.com",
+	projectId: "rps-multiplayer-72abc",
+	storageBucket: "rps-multiplayer-72abc.appspot.com",
+	messagingSenderId: "962662587269"
+};
+
+firebase.initializeApp(config);
